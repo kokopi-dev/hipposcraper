2e5406db2d3295d18822e809ce6920331653f08b
kai-dg <km.commissions@gmail.com>
2018-11-08 05:33:34 +0000

    updated header & putchar prompts, fixed no header scrape issue

--- holberton-project-scraper.py
+++ holberton-project-scraper.py
@@ -22,39 +22,42 @@ count = len(arg)
 valid_link = 'intranet.hbtn.io/projects'
 valid_header = '.h'
 
-if count > 2:
-	print("  <Error: Too many arguments (must be two)>")
-elif count == 1:
-	print("  <Error: Too few arguments (must be two)>")
-	
-if count != 2:
-	link = raw_input("Enter link to project: ")
-else:
-	link = sys.argv[1]
+if count > 1:
+        print("  <Error: Too many arguments (must be one)>")
+        sys.exit()
+elif count == 0:
+        print("  <Error: Too few arguments (must be one)>")
+        sys.exit()
+
+link = sys.argv[1]
 while not (valid_link in link):
-	print("  <Error: Invalid link (must be to project on intranet.hbtn.io)>")
-	link = raw_input("Enter link to project: ")
+        print("  <Error: Invalid link (must be to project on intranet.hbtn.io)>")
+        link = raw_input("Enter link to project: ")
 
-if count != 2:
-	header = raw_input("Enter name for project header file: ")
+# Header file prompt
+no_header = 0
+header = raw_input("Enter name for project header file (leave empty if no header required): ")
+if (header == ""):
+        no_header = 1
 else:
-	header = sys.argv[2]
-while not (header[-2:] in valid_header):
-	print("  <Error: Header file must end in .h>")
-	header = raw_input("Enter name for project header file: ")
+        while not (header[-2:] in valid_header):
+                print("  <Error: Header file must end in .h>")
+                header = raw_input("Enter name for project header file: ")
 
-# _putchar option variables and alert
+# Putchar file prompt
 putchar_list = ['y', 'n'];
 putchar_y_n = raw_input("Do you want to add _putchar.c? (y/n): ")
-if (putchar_y_n not in putchar_list):
-        print("Enter in 'y' or 'n'.")
+while not (putchar_y_n in putchar_list):
+        print(" <Error: Enter in 'y' or 'n'>")
+        putchar_y_n = raw_input("Do you want to add _putchar.c? (y/n): ")
+
 if (putchar_y_n == 'y'):
         putchar_res = 1
 if (putchar_y_n == 'n'):
         putchar_res = 0
 
 # Intranet login credentials
-with open("/CHANGE_TO_YOUR_DIRECTORY_HERE/auth_data.json", "r") as my_keys:
+with open("/home/vagrant/others/personal-scripts/personal_auth_data.json", "r") as my_keys:
         intra_keys = json.load(my_keys)
 
 # Login Variable
@@ -86,83 +89,104 @@ os.chdir(dir_name)
 
 # Making _putchar
 if (putchar_res == 1):
-	h_putchar = open("_putchar.c", "w+")
-	h_putchar.write("#include <unistd.h>\n")
-	h_putchar.write("\n")
-	h_putchar.write("/**\n")
-	h_putchar.write(" * _putchar - writes the character c to stdout\n")
-	h_putchar.write(" * @c: The character to print\n")
-	h_putchar.write(" *\n")
-	h_putchar.write(" * Return: On success 1.\n")
-	h_putchar.write(" * On error, -1 is returned, and errno is set appropriately.\n")
-	h_putchar.write(" */\n")
-	h_putchar.write("int _putchar(char c)\n")
-	h_putchar.write("{\n")
-	h_putchar.write("       return (write(1, &c, 1));\n")
-	h_putchar.write("}")
-	h_putchar.close()
+        h_putchar = open("_putchar.c", "w+")
+        h_putchar.write("#include <unistd.h>\n")
+        h_putchar.write("\n")
+        h_putchar.write("/**\n")
+        h_putchar.write(" * _putchar - writes the character c to stdout\n")
+        h_putchar.write(" * @c: The character to print\n")
+        h_putchar.write(" *\n")
+        h_putchar.write(" * Return: On success 1.\n")
+        h_putchar.write(" * On error, -1 is returned, and errno is set appropriately.\n")
+        h_putchar.write(" */\n")
+        h_putchar.write("int _putchar(char c)\n")
+        h_putchar.write("{\n")
+        h_putchar.write("       return (write(1, &c, 1));\n")
+        h_putchar.write("}")
+        h_putchar.close()
 
-# Variables for function name array
-proto_store = []
-i = 0
+if (no_header == 1):
+        i = 0
+        # Making C files with function name array
+        find_file_name = soup.find_all(string=re.compile("File: "))
+        for li in find_file_name:
+                store_file_name = open(li.next_sibling.text, "w+")
+                store_file_name.write("#include <stdio.h>\n")
+                store_file_name.write("#include <stdlib.h>\n")
+                store_file_name.write("/**\n")
+                store_file_name.write(" * main - Entry Point\n")
+                store_file_name.write(" *\n")
+                store_file_name.write(" * Return:\n")
+                store_file_name.write(" */\n")
+                store_file_name.write("int main(void)\n")
+                store_file_name.write("{\n")
+                store_file_name.write("\n")
+                store_file_name.write("}")
+                store_file_name.close()
+                i += 1
 
-# Making function name array
-find_proto = soup.find_all(string=re.compile("Prototype: "))
-for li in find_proto:
-	proto_store.append(li.next_sibling.text.replace(";", ""))
+if (no_header == 0):
+        # Variables for function name array
+        proto_store = []
+        i = 0
 
-# Making C files with function name array
-find_file_name = soup.find_all(string=re.compile("File: "))
-for li in find_file_name:
-	if (i == len(proto_store)):
-		break;
-	store_file_name = open(li.next_sibling.text, "w+")
-	store_file_name.write('#include "%s"\n\n' % header)
-	store_file_name.write("/**\n")
-	store_file_name.write(" * main - Entry Point\n")
-	store_file_name.write(" *\n")
-	store_file_name.write(" * Return:\n")
-	store_file_name.write(" */\n")
-	store_file_name.write("%s\n" % proto_store[i])
-	store_file_name.write("{\n")
-	store_file_name.write("\n")
-	store_file_name.write("}")
-	store_file_name.close()
-	i += 1
+        # Making function name array
+        find_proto = soup.find_all(string=re.compile("Prototype: "))
+        for li in find_proto:
+                proto_store.append(li.next_sibling.text.replace(";", ""))
 
-# Variables for header prototypes array
-proto_h_store = []
-n = 0
+        # Making C files with function name array
+        find_file_name = soup.find_all(string=re.compile("File: "))
+        for li in find_file_name:
+                if (i == len(proto_store)):
+                        break;
+                store_file_name = open(li.next_sibling.text, "w+")
+                store_file_name.write('#include "%s"\n\n' % header)
+                store_file_name.write("/**\n")
+                store_file_name.write(" * main - Entry Point\n")
+                store_file_name.write(" *\n")
+                store_file_name.write(" * Return:\n")
+                store_file_name.write(" */\n")
+                store_file_name.write("%s\n" % proto_store[i])
+                store_file_name.write("{\n")
+                store_file_name.write("\n")
+                store_file_name.write("}")
+                store_file_name.close()
+                i += 1
 
-# Find header prototype
-find_proto_h = soup.find_all(string=re.compile("Prototype: "))
-for li in find_proto_h:
-        proto_h_store.append(li.next_sibling.text)
+        # Variables for header prototypes array
+        proto_h_store = []
+        n = 0
 
-# Making header include guard string
-include_guard = header 
-include_guard = include_guard.replace('.', '_', 1)
-include_guard = include_guard.upper()
+        # Find header prototype
+        find_proto_h = soup.find_all(string=re.compile("Prototype: "))
+        for li in find_proto_h:
+                proto_h_store.append(li.next_sibling.text)
 
-# Making header file
-make_header = open(sys.argv[2], "w+")
-make_header.write('#ifndef %s\n' % include_guard)
-make_header.write('#define %s\n' % include_guard)
-make_header.write("\n")
-make_header.write("#include <stdio.h>\n")
-make_header.write("#include <stdlib.h>\n")
-make_header.write("\n")
+        # Making header include guard string
+        include_guard = header
+        include_guard = include_guard.replace('.', '_', 1)
+        include_guard = include_guard.upper()
 
-if (putchar_res == 1):
-	make_header.write("int _putchar(char c);\n")
+        # Making header file
+        make_header = open(header, "w+")
+        make_header.write('#ifndef %s\n' % include_guard)
+        make_header.write('#define %s\n' % include_guard)
+        make_header.write("\n")
+        make_header.write("#include <stdio.h>\n")
+        make_header.write("#include <stdlib.h>\n")
+        make_header.write("\n")
 
-for li in find_proto_h:
-	if (n == len(proto_h_store)):
-		break;
-	make_header.write(proto_h_store[n])
-	make_header.write("\n")
-	n += 1
+        if (putchar_res == 1):
+                make_header.write("int _putchar(char c);\n")
 
-make_header.write("\n")
-make_header.write('#endif /* %s */' % include_guard)
-make_header.close()
+        for li in find_proto_h:
+                if (n == len(proto_h_store)):
+                        break;
+                make_header.write(proto_h_store[n])
+                make_header.write("\n")
+                n += 1
+
+        make_header.write("\n")
+        make_header.write('#endif /* %s */' % include_guard)
+        make_header.close()
