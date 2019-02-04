4286df7dfc67250c11714cb3e9fec5e0aca04bdc
bdbaraban <375@holbertonschool.com>
2019-02-04 03:27:22 +0000

    Update module-level documentation and improve error messages

--- scrapers/bash_scraper.py
+++ scrapers/bash_scraper.py
@@ -1,15 +1,23 @@
-#!/usr/bin/python2
+#!/usr/bin/env bash
+"""Defines the file creator for system engineering projects."""
 import sys
 
+
 def scrape_bash(find_file_name):
+    """Create Bash script files for system engineering projects.
+
+    Files are created with shebangs.
+
+    Args:
+        find_file_name (list): A list of task file names.
+    """
     for li in find_file_name:
         try:
-            make_file = open(li.next_sibling.text, "w+")
+            make_file = open(li.next_sibling.text, "w")
             make_file.write("#!/usr/bin/env bash\n")
             make_file.close()
-        except:
-            sys.stdout.write("Error: Could not create ")
+        except (AttributeError, IndexError):
+            sys.stdout.write("[ERROR] Failed to create ")
             sys.stdout.write("task file %s\n" % text_file)
-            sys.stdout.write("                   ... ")
+            sys.stdout.write("                        ... ")
             continue
-
